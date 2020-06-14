f5589bad63dbf01afd954dd23497d85aa6c8caca
jaebin1234 <jaebin2586@gmail.com>
2020-06-14 21:38:58 +0900

    구구단을만들어보자.

---
+++ src/com/company/gugudan_2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/*구구단 구현*/
+
+public class gugudan_2 {
+    public static void main(String[] args) {
+        //4단
+        System.out.println("4단");
+           int result = 4*1;
+        System.out.println(result);
+
+        }
+    }
+
